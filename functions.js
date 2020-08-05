21f065663df4f93be953ccb1c2227fc51e6dc122
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-08-05 04:11:18 +0000

    Functions, Done, Objects and Further study started.

--- functions.js
+++ functions.js
@@ -21,5 +21,22 @@ function getFullName(firstName, lastName){
 
 
 // 3. calculateTotal
+function calculateTotal(state, basePrice, tax = 0.05) {
+  const subtotal = basePrice * (1 + tax);
+  let fee = 0;
+
+  if (state === "CA") {
+    fee = 0.03 * subtotal;
+  } else if (state === "PA") {
+    fee = 2;
+  } else if (state === "MA") {
+    if (basePrice <= 100) {
+      fee = 1;
+    } else {
+      fee = 3;
+    }
+  }
+  return subtotal + fee;
+}
 
 
--- furtherStudy.js
+++ furtherStudy.js
@@ -2,9 +2,15 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
-}
 
+  const result = [];
+  for (const word of words1) {
+    console.log(word);
+    if (words2.includes(word)) {
+      result.push(word);
+    }
+  }
+  return result;
 
 function kidsGame(names) {
   // Replace this with your code
